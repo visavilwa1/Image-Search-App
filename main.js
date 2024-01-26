32150cc223bef5885aac56744e36f586c5f2f6f5
Visavilwa Daisy <135525440+visavilwa1@users.noreply.github.com>
2024-01-27 00:06:19 +0300

    commit

--- main.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-document.addEventListener("DOMContent", function (){
-    cont menuItems = document.querySelectorAll("menu-item");
-=======
-document.addEventListener("DOMContent", function (){
-    cont menuItems = document.querySelectorAll("menu-item");
->>>>>>> fe17662db72a3528942cd1bbda8e90438b82b780
-})
